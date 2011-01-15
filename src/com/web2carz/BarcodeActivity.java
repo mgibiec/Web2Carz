eae77860ef5a5cffa446f102c69bc0c357375144
gczajkow <gczajkow@GCZAJKOW-MOBL2.amr.corp.intel.com>
2011-01-15 13:45:03 -0600

    added barcode scanner

---
+++ src/com/web2carz/BarcodeActivity.java
@@ -0,0 +1,16 @@
+package com.web2carz;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BarcodeActivity extends Activity {
+	/**
+	 * @see android.app.Activity#onCreate(Bundle)
+	 */
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.buy_car);
+		// TODO Put your code here
+	}
+}
--- src/com/web2carz/BuyCarActivity.java
+++ src/com/web2carz/BuyCarActivity.java
@@ -1,7 +1,13 @@
 package com.web2carz;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.content.Intent;
 
 public class BuyCarActivity extends Activity {
 	/**
@@ -11,6 +17,23 @@ public class BuyCarActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.buy_car);
-		// TODO Put your code here
+	}
+
+	public void scanBarcode(View v) {
+		ZxingIntentIntegrator.initiateScan(this);
+
+	}
+
+	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+		ZxingIntentResult res = ZxingIntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+		if (res != null) {
+			Toast.makeText(this, res.getContents(),	Toast.LENGTH_LONG).show();
+			Intent webIntent = new Intent(Intent.ACTION_VIEW,
+					Uri.parse(res.getContents()));
+			startActivity(webIntent);
+		}
+		else {
+
+		}
 	}
 }
